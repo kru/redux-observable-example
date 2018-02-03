820752c44570b16b404186069b0d201fdff2fe27
Krisrp <kristoforusrp@gmail.com>
2018-02-03 20:37:26 +0700

    update reducerEpics

--- src/reducerEpics.js
+++ src/reducerEpics.js
@@ -2,10 +2,14 @@ import { combineEpics } from 'redux-observable';
 import { combineReducers } from 'redux-immutable';
 
 import mainScreenReducer from './containers/MainScreen/reducer';
+import userRepoReducer from './containers/SearchScreen/reducer';
+
 import { fetchUsersEpic } from './containers/MainScreen/actions';
+import { fetchUserReposEpic } from './containers/SearchScreen/actions';
 
 export const rootReducer = combineReducers({
-    mainScreen: mainScreenReducer
+    mainScreen: mainScreenReducer,
+    userRepository: userRepoReducer
 });
 
-export const rootEpic = combineEpics(fetchUsersEpic);
+export const rootEpic = combineEpics(fetchUsersEpic, fetchUserReposEpic);
