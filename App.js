19a2f234234d26bab9003ba61599aa4198ff1910
Krisrp <kristoforusrp@gmail.com>
2018-02-03 20:37:07 +0700

    add search screen component

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { Provider } from 'react-redux';
 
 import store from './src/store';
 import MainScreen from './src/containers/MainScreen';
+import SearchScreen from './src/containers/SearchScreen';
 
 export default class App extends Component<{}> {
     render() {
         return (
             <Provider store={store}>
-                <MainScreen />
+                <SearchScreen />
             </Provider>
         );
     }
